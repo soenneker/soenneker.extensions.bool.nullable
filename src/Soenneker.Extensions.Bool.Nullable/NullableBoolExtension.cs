a4acd0fbacf2eaf90840cd7eb244b5e9f338a190
Jake Soenneker <jake@soenneker.com>
2026-04-02 08:45:39 -0500

    addt methods

--- src/Soenneker.Extensions.Bool.Nullable/NullableBoolExtension.cs
+++ src/Soenneker.Extensions.Bool.Nullable/NullableBoolExtension.cs
@@ -17,4 +17,24 @@ public static class NullableBoolExtension
     {
         return value != true;
     }
+
+    /// <returns>"Yes" or "No" or "null"</returns>
+    [Pure]
+    public static string ToYesNoNullable(this bool? value)
+    {
+        if (!value.HasValue)
+            return "Null";
+
+        return value.Value ? "Yes" : "No";
+    }
+
+    /// <returns>"Yes" or "No" or "null"</returns>
+    [Pure]
+    public static string ToYesNoNullableLower(this bool? value)
+    {
+        if (!value.HasValue)
+            return "null";
+
+        return value.Value ? "yes" : "no";
+    }
 }
